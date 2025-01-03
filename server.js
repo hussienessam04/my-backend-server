5a434ac86aa82c8ecf0855e7e4eef2c62b9ab3c7
hussienessam04 <hussienessam047@gmail.com>
2025-01-03 18:21:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080; // Use port 8080
 
 app.use(cors());
 app.use(express.json());
